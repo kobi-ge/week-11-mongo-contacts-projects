88190318f485924a855c17d7aa0da5ca5f94612c
kobi <kodigershuni@gmail.com>
2025-12-30 14:17:35 +0200

    select and insert methods

--- app/data_interactor.py
+++ app/data_interactor.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 import os
 from dotenv import load_dotenv
 
@@ -10,10 +11,39 @@ DB = os.getenv("MONGO_DB")
 
 class MongodbInstance:
     def __init__(self):
-        self.client = MongoClient(host= HOST, port=PORT)
+        self.db =None
+        self.collection = None
 
-    def get_collection(self):
-        database = self.client[DB]
-        collection = database['contacts']
-        return collection
+    def get_database(self):
+        try:
+            client = MongoClient(f"mongodb://{HOST}:{PORT}")
+            client.admin.command("ping")
+            self.db = client.contacts_db
+            self.collection = self.db.contacts
+            return "Successfully connected to MongoDB"
+        
+        except ConnectionFailure as e:
+            return f"failed to connect to db: {e}"
 
+
+    def select(self, id = None):
+        if self.db is None:
+            self.get_database()
+        return list(self.collection.find())
+
+    def insert(self, contact):
+        contact = contact.to_dict()
+        result = self.db.contacts.insert_one(contact)
+        return result.inserted_id
+
+    def update(collection, identifier):
+        pass
+
+    def delete(collection, identifier):
+        pass
+
+
+a = MongodbInstance()
+a.get_database()
+print(a.collection)
+print(a.select())
--- app/main.py
+++ app/main.py
@@ -1,16 +1,45 @@
 from fastapi import FastAPI, HTTPException
 import uvicorn
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
-class ContactSchema(BaseModel):
-    first_name: str
-    last_name: str
-    phone_number: str
+import data_interactor as di
+
+
+
+class Contact(BaseModel):
+    id: int = Field(..., alias= id)
+    first_name: str = Field(..., max_length=50)
+    last_name: str = Field(..., max_length=50)
+    phone_number: str = Field(..., max_length=20)
+
+    def to_dict(self):
+        return {
+                "first_name": self.first_name, 
+                "last_name": self.last_name, 
+                "phone_number": self.phone_number
+                }
 
 app = FastAPI()
+mongodb = di.MongodbInstance()
+mongodb.get_database()
 
-@app.get("/")
 
+
+@app.get("/contacts")
+def get_all_contacts():
+    pass
+
+@app.post("/contacts")
+def post_new_contact(contact: Contact):
+    return mongodb.insert(contact)
+
+@app.update("/contacts/{id}")
+def update_contact():
+    pass
+
+@app.delete("/contacts/{id}")
+def delete_contact(id: int):
+    pass
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
